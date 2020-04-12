6ade65df103aecb4d23700a6727326ee8d4bf9ba
YuriiTrofimenko <tyaa@ukr.net>
2020-04-12 19:13:19 +0300

    Adding new Items

--- AvaloniaMVVMTodoDemo/ViewModels/MainWindowViewModel.cs
+++ AvaloniaMVVMTodoDemo/ViewModels/MainWindowViewModel.cs
@@ -1,17 +1,49 @@
-﻿using AvaloniaMVVMTodoDemo.Services;
+﻿using AvaloniaMVVMTodoDemo.Models;
+using AvaloniaMVVMTodoDemo.Services;
+using ReactiveUI;
 using System;
 using System.Collections.Generic;
+using System.Reactive.Linq;
 using System.Text;
 
 namespace AvaloniaMVVMTodoDemo.ViewModels
 {
     public class MainWindowViewModel : ViewModelBase
     {
+        ViewModelBase content;
+
         public MainWindowViewModel(Database db)
         {
-            List = new TodoListViewModel(db.GetItems());
+            Content = List = new TodoListViewModel(db.GetItems());
+        }
+
+        public ViewModelBase Content
+        {
+            get => content;
+            private set => this.RaiseAndSetIfChanged(ref content, value);
         }
 
         public TodoListViewModel List { get; }
+
+        public void AddItem()
+        {
+            var vm = new AddItemViewModel();
+
+            Observable.Merge(
+                vm.Ok,
+                vm.Cancel.Select(_ => (TodoItem)null))
+                .Take(1)
+                .Subscribe(model =>
+                {
+                    if (model != null)
+                    {
+                        List.Items.Add(model);
+                    }
+
+                    Content = List;
+                });
+
+            Content = vm;
+        }
     }
 }
