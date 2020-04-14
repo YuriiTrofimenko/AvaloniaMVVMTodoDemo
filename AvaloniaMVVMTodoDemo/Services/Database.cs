14dd597aba8853614fea49d1da589e75f468cf97
YuriiTrofimenko <tyaa@ukr.net>
2020-04-14 11:51:37 +0300

    Empty List Placeholder

--- AvaloniaMVVMTodoDemo/Services/Database.cs
+++ AvaloniaMVVMTodoDemo/Services/Database.cs
@@ -7,11 +7,12 @@ namespace AvaloniaMVVMTodoDemo.Services
 {
     public class Database
     {
-        public IEnumerable<TodoItem> GetItems() => new[]
+        /*public IEnumerable<TodoItem> GetItems() => new[]
         {
             new TodoItem { Description = "Walk the dog" },
             new TodoItem { Description = "Buy some milk" },
             new TodoItem { Description = "Learn Avalonia", IsChecked = true },
-        };
+        };*/
+        public IEnumerable<TodoItem> GetItems() => new TodoItem[0];
     }
 }
